0fcfc5ac07bff9fad584445a01cb0f75f99f267d
unknown <archernich@gmail.com>
2021-06-27 23:36:13 -0700

    change for testing

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const {ApolloServer} = require('apollo-server-express');
-const {typeDefs, resolvers} = require('./schemas');
-const {authMiddleware} = require('./utils/auth');
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
 
 const path = require('path');
 const db = require('./config/connection');
@@ -16,7 +16,7 @@ const server = new ApolloServer({
   context: authMiddleware
 });
 
-server.applyMiddleware({app});
+server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -28,11 +28,16 @@ if (process.env.NODE_ENV === 'production') {
 
 app.use(express.static('public'));
 
-app.use(routes);
+// app.use(routes);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`🌍 Now listening on localhost:${PORT}`);
     console.log(`GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   }
-)});
+  )
+});
